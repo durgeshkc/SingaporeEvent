4237ad8f90e3e22a239378105f3daef42ce0a179
Trung, Nguyen Thanh <thanhtnguyen@paypal.com>
2015-02-04 09:46:25 +0800

    Implement simple Google Map API function

--- resolveaddress/src/main/java/paypal/resolveaddress/MainActivity.java
+++ resolveaddress/src/main/java/paypal/resolveaddress/MainActivity.java
@@ -1,10 +1,27 @@
 package paypal.resolveaddress;
 
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -14,8 +31,19 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText addressField = (EditText) findViewById(R.id.addressField);
+        final EditText addressField = (EditText) findViewById(R.id.addressField);
         addressField.setHint("Enter your address");
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Log.i("test", addressField.getText().toString());
+                HttpAsyncTask asyncTask = new HttpAsyncTask();
+                asyncTask.execute(addressField.getText().toString());
+            }
+        });
     }
 
 
@@ -39,5 +67,51 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    class HttpAsyncTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) { // Run in own threads
+            String response = "";
+            for (String place : params) {
+                DefaultHttpClient client = new DefaultHttpClient();
+                HttpGet httpGet = null;
+                try {
+                    httpGet = new HttpGet(
+                            "http://maps.google.com/maps/api/geocode/json?address="
+                                    + URLEncoder.encode(place, "utf-8")
+                                    + "&sensor=false");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    HttpResponse execute = client.execute(httpGet);
+                    InputStream content = execute.getEntity().getContent();
+
+                    BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                    String s;
+                    while ((s = buffer.readLine()) != null) {
+                        response += s;
+                    }
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String s) { // Run in user interface thread
+            super.onPostExecute(s);
+            try {
+                JSONObject topObject = new JSONObject(s);
+                JSONArray resArr = (JSONArray)topObject.get("results");
+                TextView tv = (TextView) findViewById(R.id.result);
+                tv.setText("Formatted Address: " + resArr.getJSONObject(0).getString("formatted_address"));
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 }
