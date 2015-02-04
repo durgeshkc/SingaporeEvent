334617eaf692a2c1dd0553af1dfab5d17e2e07a0
Trung, Nguyen Thanh <thanhtnguyen@paypal.com>
2015-02-04 14:37:14 +0800

    Play with Jsoup

--- firstmap/src/main/java/paypal/firstmap/JsoupTest.java
+++ firstmap/src/main/java/paypal/firstmap/JsoupTest.java
@@ -2,6 +2,7 @@ package paypal.firstmap;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
@@ -11,8 +12,12 @@ import java.io.IOException;
  */
 public class JsoupTest {
     public static void main(String[] args) throws IOException {
-        Document doc = Jsoup.connect("http://en.wikipedia.org/").get();
-        Elements newsHeadlines = doc.select("#mp-itn b a");
-        System.out.println(newsHeadlines);
+        Document doc = Jsoup.connect("http://www.worldtravelguide.net/singapore-city/events").get();
+        Elements eventDivs = doc.select("div[class=poi-list]");
+        for (Element eventDiv : eventDivs) {
+            System.out.print(eventDiv.child(1).child(0).text());
+            System.out.print("   (" + eventDiv.child(2).ownText() + ")    ");
+            System.out.println(eventDiv.child(4).child(1).ownText());
+        }
     }
 }
