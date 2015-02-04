e0742abb75e0f7048cb5130229478e27a76c5986
Trung, Nguyen Thanh <thanhtnguyen@paypal.com>
2015-02-04 14:43:42 +0800

    Update Jsoup code

--- firstmap/src/main/java/paypal/firstmap/JsoupTest.java
+++ firstmap/src/main/java/paypal/firstmap/JsoupTest.java
@@ -5,19 +5,26 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.IOException;
-
-/**
- * Created by thanhtnguyen on 4/2/15.
- */
 public class JsoupTest {
-    public static void main(String[] args) throws IOException {
-        Document doc = Jsoup.connect("http://www.worldtravelguide.net/singapore-city/events").get();
-        Elements eventDivs = doc.select("div[class=poi-list]");
-        for (Element eventDiv : eventDivs) {
-            System.out.print(eventDiv.child(1).child(0).text());
-            System.out.print("   (" + eventDiv.child(2).ownText() + ")    ");
-            System.out.println(eventDiv.child(4).child(1).ownText());
+
+    public static void main(String s[]){
+        try {
+            Document doc = Jsoup.connect("https://www.eventbrite.sg/d/singapore--singapore/events/").get();
+            //System.out.println(doc.toString());
+            Elements elements = doc.getElementsByAttributeValue("itemprop","startDate");
+            Elements elementsLocation = doc.getElementsByAttributeValue("itemprop","location");
+            for (int i=0;i<elements.size();i++){
+                //System.out.println(elements.get(i));
+                Element element = elements.get(i);
+                String dateValue = element.text();
+                Element elementLocation = elementsLocation.get(i);
+                String locationValue = elementLocation.text();
+                System.out.println(dateValue + " == "+locationValue);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
         }
+
     }
+
 }
